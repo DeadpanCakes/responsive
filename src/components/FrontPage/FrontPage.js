a4d88a8c27790dc1068a23395288983502131808
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-03 18:52:56 -0700

    added articlecards to front page and styled with grid

--- src/components/FrontPage/FrontPage.js
+++ src/components/FrontPage/FrontPage.js
@@ -1,7 +1,37 @@
 import "./FrontPage.css";
+import "../ArticleCard/ArticleCard";
+import ArticleCard from "../ArticleCard/ArticleCard";
 
 const FrontPage = () => {
-  return <section className="frontPage"></section>;
+  return (
+    <section className="frontPage">
+      <ArticleCard
+        link="google.com"
+        topic="Facial Recognition"
+        title="There's something fishy about Facebook's pivot on facial recognition"
+      />
+      <ArticleCard
+        link="google.com"
+        topic="Facial Recognition"
+        title="There's something fishy about Facebook's pivot on facial recognition"
+      />
+      <ArticleCard
+        link="google.com"
+        topic="Facial Recognition"
+        title="There's something fishy about Facebook's pivot on facial recognition"
+      />
+      <ArticleCard
+        link="google.com"
+        topic="Facial Recognition"
+        title="There's something fishy about Facebook's pivot on facial recognition"
+      />
+      <ArticleCard
+        link="google.com"
+        topic="Facial Recognition"
+        title="There's something fishy about Facebook's pivot on facial recognition"
+      />
+    </section>
+  );
 };
 
 export default FrontPage;
