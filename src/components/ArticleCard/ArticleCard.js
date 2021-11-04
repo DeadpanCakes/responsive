e9cf917f909fd9ea4b59b91002516ed87aba8d8c
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-03 19:27:15 -0700

    restructured article card to make whole region clickable

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -2,16 +2,16 @@ import "./ArticleCard.css";
 
 const ArticleCard = ({ link, topic, title, img }) => {
   return (
-    <article>
-      <a href={link}>
+    <a href={link} className="articleCard">
+      <article>
         <div style={{ background: `url(${img}) blue` }}>
           <div className="articleCardContent">
             <p>{topic}</p>
             <h2>{title}</h2> from "./PageFooter";
           </div>
         </div>
-      </a>
-    </article>
+      </article>
+    </a>
   );
 };
 
