b886034d6c833207b93ef02de24c46608e0e8f30
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-05 20:25:41 -0700

    added content and styled

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,7 +1,24 @@
 import "./Newsletter.css";
 
 const Newsletter = () => {
-  return <form className="newsletter"></form>;
+  return (
+    <form className="newsletter">
+      <h2>Get the snarkiest newsletter in tech</h2>
+      <h3>
+        Subscribe to our twice weekly{" "}
+        <a href="https://share.hsforms.com/1eT1w-z6ZSAiGIVEzvR5QMw47gef">
+          Big Spam
+        </a>{" "}
+        or check out our{" "}
+        <a href="https://thenextweb.com/newsletters">other newsletters.</a>
+      </h3>
+      <label className="newsletterLabel" for="newsletterInput">
+        Email
+      </label>
+      <input id="newsletterInput" placeholder="Email" />
+      <button>SUBSCRIBE</button>
+    </form>
+  );
 };
 
 export default Newsletter;
