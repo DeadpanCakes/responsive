8c65e1884b291c5ab094e363c3e6cd7561b47705
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-08 20:09:25 -0800

    added content to pageheader and styled

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,7 +1,36 @@
 import "./PageHeader.css";
 
 const PageHeader = () => {
-  return <header className="pageHeader"></header>;
+  return (
+    <header className="pageHeader">
+      <nav className="headerNav">
+        <li>
+          <a href="https://thenextweb.com/">TNW</a>
+        </li>
+        <li>
+          <a href="https://thenextweb.com/">HOME</a>
+        </li>
+        <li>
+          <a href="https://thenextweb.com/">NEWS</a>
+        </li>
+        <li>
+          <a href="https://thenextweb.com/">EVENTS</a>
+        </li>
+        <li>
+          <a href="https://thenextweb.com/programs">PROGRAMS</a>
+        </li>
+        <li>
+          <a href="https://thenextweb.com/spaces">SPACES</a>
+        </li>
+        <li>
+          <a href="https://thenextweb.com/partnerships">PARTNER WITH US</a>
+        </li>
+        <li>
+          <a href="https://thenextweb.com/partnerships">MAIL</a>
+        </li>
+      </nav>
+    </header>
+  );
 };
 
 export default PageHeader;
