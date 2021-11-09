484d7c82be12c7ca64c9654af3026d786807b734
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-09 15:51:44 -0800

    added content to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import "./App.css";
+import PageHeader from "./components/PageHeader/PageHeader";
+import FrontPage from "./components/FrontPage/FrontPage";
+import Popular from "./components/Popular/Popular";
+import Latest from "./components/Latest/Latest";
+import Channels from "./components/Channels/Channels";
+import Newsletter from "./components/Newsletter/Newsletter";
+import Videos from "./components/Videos/Videos";
+import Contact from "./components/Contact/Contact";
+import PageFooter from "./components/PageFooter/PageFooter";
 
 function App() {
   return (
     <div className="App">
+      <PageHeader />
+      <FrontPage />
+      <Popular />
+      <Latest />
+      <Channels />
+      <Newsletter />
+      <Videos />
+      <Contact />
+      <PageFooter />
     </div>
   );
 }
