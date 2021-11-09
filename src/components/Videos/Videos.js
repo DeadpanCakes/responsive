d7f603ad31886a78000442e5a93e9785f2822e99
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-08 20:58:56 -0800

    styled videos

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -5,7 +5,7 @@ import VidCard from "./VidCard";
 const Videos = () => {
   return (
     <section className="videos">
-      <h2>Watch what we made you</h2>
+      <h2 className="vidHeader">Watch what we made you</h2>
       <MainVid
         vidSrc="https://www.youtube.com/embed/S1-wuoFsdT4"
         title="Nothing Ear (1) review: Style... and substance?"
