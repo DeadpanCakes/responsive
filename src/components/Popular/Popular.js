fe6ee1745dff8a3484a91c77f029ea0d58e71a59
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-08 18:56:01 -0800

    added content

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -1,7 +1,19 @@
 import "./Popular.css";
+import ArticleCardSm from "../ArticleCard/ArticleCardSm";
 
 const Popular = () => {
-  return <section className="popular"></section>;
+  return (
+    <section className="popular">
+      <h2>Popular Articles</h2>
+      <ol className="articleList">
+        <ArticleCardSm title="This pop-up cabin is a huge step up over that cramped little tent." />
+        <ArticleCardSm title="The Volvo C40 shows how luxury cars can be green" />
+        <ArticleCardSm title="The Bluesound Node is the music streamer every Hi-Fi setup needs." />
+        <ArticleCardSm title="Microsoft's metaverse doesn't have legs -- literally." />
+        <ArticleCardSm title="Here's why invesment in ebikes is booming." />
+      </ol>
+    </section>
+  );
 };
 
 export default Popular;
