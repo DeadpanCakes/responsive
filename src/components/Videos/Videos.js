3f34fb593e2d868fda5ef9610b06bf62a95c2e13
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-08 20:48:24 -0800

    imported mainvid

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,10 +1,15 @@
 import "./Videos.css";
+import MainVid from "./MainVid";
 
 const Videos = () => {
   return (
     <section className="videos">
       <h2>Watch what we made you</h2>
-      <video></video>
+      <MainVid
+        vidSrc="https://www.youtube.com/embed/S1-wuoFsdT4"
+        title="Nothing Ear (1) review: Style... and substance?"
+        date="Jul 30, 2021"
+      />
       <ul className="videoCarousel"></ul>
     </section>
   );
