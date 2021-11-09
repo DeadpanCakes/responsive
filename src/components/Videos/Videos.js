69b75cc8b93208911ab85cc78fae10f4d2ce90d4
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-08 20:37:15 -0800

    added skeleton to videos

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,7 +1,13 @@
 import "./Videos.css";
 
 const Videos = () => {
-  return <section className="videos"></section>;
+  return (
+    <section className="videos">
+      <h2>Watch what we made you</h2>
+      <video></video>
+      <ul className="videoCarousel"></ul>
+    </section>
+  );
 };
 
 export default Videos;
