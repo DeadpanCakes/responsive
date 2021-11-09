c6c5e614433ceae704247cf677165fee15acd94e
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-08 20:29:50 -0800

    styled contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,16 +3,18 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <section className="contact">
-      <h2>Work with us</h2>
-      <p>
-        TNW takes center stage in the tech industry, offering creative media
-        campaigns, sizzling tech events, bespoke innovation programs, and prime
-        office locations in Amsterdam. Want to engage your audience more
-        effectively, innovate your business, and position your brand in front of
-        a digital-savvy audience?
-      </p>
-      <button>Get In Touch</button>
-      <div>
+      <div className="contactContent">
+        <h2>Work with us</h2>
+        <p>
+          TNW takes center stage in the tech industry, offering creative media
+          campaigns, sizzling tech events, bespoke innovation programs, and
+          prime office locations in Amsterdam. Want to engage your audience more
+          effectively, innovate your business, and position your brand in front
+          of a digital-savvy audience?
+        </p>
+        <button>Get In Touch</button>
+      </div>
+      <div className="contactIdeas">
         <div>Media</div>
         <div>Events</div>
         <div>Spaces</div>
