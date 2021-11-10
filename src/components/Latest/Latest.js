d90d4f906f74e56605e28bc89cb419bd5fd1564d
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-09 16:51:10 -0800

    replaced placeholder content

--- src/components/Latest/Latest.js
+++ src/components/Latest/Latest.js
@@ -9,56 +9,56 @@ const Latest = () => {
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="Expanding black holes could be playing Pac Man with the universe"
           img={null}
           classes={['latestStories']}
         />
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="Why Elon Musk is an idiot"
           img={null}
           classes={['latestStories']}
         />
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="Your Spotify Wrapped 2021 is embarassing and there's nothing you can do about it"
           img={null}
           classes={['latestStories']}
         />
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="Gig startup want you to believe they can replace your job-- don't fall for it"
           img={null}
           classes={['latestStories']}
         />
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="Paging the plagarism police-- AI is getting better at writing students' essays"
           img={null}
           classes={['latestStories']}
         />
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="The Facebook whistleblower must-have rules to rein in social media"
           img={null}
           classes={['latestStories']}
         />
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="Here are 10 awesome deals on power generators and solar panels at special pre-Black Friday prices"
           img={null}
           classes={['latestStories']}
         />
         <ArticleCard
           link="google.com"
           topic="Facial Recognition"
-          title="There's something fishy about Facebook's pivot on facial recognition"
+          title="Would you buy a car without its promised tech? BMW latest to make compromises due to chip shortage"
           img={null}
           classes={['latestStories']}
         />
