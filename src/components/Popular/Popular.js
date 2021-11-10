872c4a889e67d0586804ef4102fe9e36c705525a
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-09 16:40:55 -0800

    adjusted sizing and colors

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -4,7 +4,7 @@ import ArticleCardSm from "../ArticleCard/ArticleCardSm";
 const Popular = () => {
   return (
     <section className="popular">
-      <h2>Popular Articles</h2>
+      <h2 className="popularHeading">Popular Articles Today</h2>
       <ol className="articleList">
         <ArticleCardSm title="This pop-up cabin is a huge step up over that cramped little tent." />
         <ArticleCardSm title="The Volvo C40 shows how luxury cars can be green" />
